728cdb478b6df119d23bfa25c09478c7c0c2277e
Austin <pizzahi5@gmail.com>
2023-03-08 11:17:08 -0800

    Finished_hw7

--- src/Homeworks/Homework7.sol
+++ src/Homeworks/Homework7.sol
@@ -3,9 +3,14 @@
 pragma solidity ^0.8.13;
 
 contract Homework7 {
+
+    event TransferOccurred(address receiver, uint256 amount);
+
+    bytes blah;
+
     // write tests for these
     function query(bytes memory data, function(uint) external test)
-    public pure {
+    public {
         //look at selector of passed in function, check theres no IERC20 
         assembly {
             if eq(test.selector, 0xa9059cbb) {
@@ -15,9 +20,16 @@ contract Homework7 {
                 revert(ptr, size)
             }
         }
+
+        //this is here just to get rid of that yellow warning
+        blah = data;
     }
 
-    function checkCall(bytes calldata data) external{
-    
+    function checkCall(bytes calldata data) external {
+        (bytes4 selector, address target, uint256 amount) = 
+            abi.decode(data, (bytes4,address,uint256));
+
+        require(selector == 0xa9059cbb, "Not ERC20 Transfer");
+        emit TransferOccurred(target, amount);
     }
 }
---
+++ test/Homework7.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Homeworks/Homework7.sol";
+
+contract Homework7Test is Test {
+    Homework7 public eg;
+
+    function setUp() public {
+        eg = new Homework7();
+    }
+
+    function testQuery() public {
+
+    }
+
+    function testCheckCall() public {
+        
+    }
+
+}
